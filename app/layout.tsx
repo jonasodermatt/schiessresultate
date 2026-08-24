0d5cb14c425a48e556b396f527828390673002ec
Jonas Odermatt <jonas_odermatt@gmx.ch>
2026-08-24 19:52:49 +0200

    Logo wieder eingefügt

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "EasyShooter",
   description: "Schiessresultate erfassen, verwalten und auswerten.",
+    icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
